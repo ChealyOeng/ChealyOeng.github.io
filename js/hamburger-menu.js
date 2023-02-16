1a5b9a1344373dc9059fe9c98193d5885f9aec1d
Yiannis Charalambous <yiannis128@hotmail.com>
2023-02-16 20:27:41 +0000

    Update

--- js/hamburger-menu.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-    const menuButton = document.querySelector("#menuButton");
-    const nav = document.getElementsByTagName("nav")[0];
-    menuButton.addEventListener("click", () => {
-        var isActive = menuButton.classList.toggle("Active");
-        nav.classList.toggle("Active", isActive);
-    });
-};
